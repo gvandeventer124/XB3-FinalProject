a50e5d53f4b58faffce923bf11b52e70ced4e1ed
Graham Van Deventer <gvandeventer124@gmail.com>
2016-03-31 17:27:06 -0400

    Added user input

--- Control.java
+++ Control.java
@@ -1,17 +1,19 @@
 package sortingForFinalProject;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Scanner;
 public class Control {
 
 	static WeatherEvent[] year06, year07, year08, year09, year10, year11, year12, 
 	year13, year14, year15;
 	
 	public static void main(String[] args) {
-
-		
 		// TODO Auto-generated method stub
 		int [] days = {31,28,30,31,30,31,30,31,30,31,30,31};
+		//Extraction
+		System.out.print("");
 		UserInputState();
 		UserInputCity();
-		Extraction.main(null);
 		SortByDate();
 		for(int i = 0; i < days.length; i++){
 			for(int j = 1; j <= days[i]; j++){
@@ -20,14 +22,59 @@ public class Control {
 			}
 		}
 	}
-	public static WeatherEvent[] UserInputState(){
-		System.out.println("Extracting all WeatherEvents for selected State");
-		return new WeatherEvent[0];
+	public static String UserInputState(){
+		Scanner stateread = new Scanner(System.in);
+		boolean invalid = true;
+		int year = 2006;
+		String state = stateread.nextLine();
+		while(invalid && year < 2016){
+			try{
+				File f = new File("Data/StormEvents-"+year+".csv"); // Read from the input file
+				Scanner s = new Scanner(f);
+				while(s.hasNextLine()){
+					String line = s.nextLine();
+					String[] brokenline = line.split(",");
+					if(brokenline[8].compareToIgnoreCase('"'+state+'"') == 0){
+						return state;
+					}
+				}
+				year++;
+			}
+			catch(Exception e){
+				
+			}
+		}
+		System.out.println("Invalid input of state. Please Try again");
+		return UserInputState();
 	}
-	public static WeatherEvent[] UserInputCity(){
-		System.out.println("Extracting all WeatherEvents for selected City");
-		return new WeatherEvent[0];
+	public static String UserInputCity(){
+		Scanner cityread = new Scanner(System.in);
+		boolean invalid = true;
+		int year = 2006;
+		String city = cityread.nextLine();
+		while(invalid && year < 2016){
+			try{
+				File f = new File("Data/StormEvents-"+year+".csv"); // Read from the input file
+				Scanner s = new Scanner(f);
+				while(s.hasNextLine()){
+					String line = s.nextLine();
+					String[] brokenline = line.split(",");
+					if(brokenline[15].compareToIgnoreCase('"'+city+'"') == 0){
+						return city;
+						//System.out.println(brokenline[15]);
+					}
+				}
+				year++;
+			}
+			catch(Exception e){
+				
+			}
+		}
+		return"";
+		//System.out.println("Invalid input of state. Please Try again");
+		//return UserInputCity();
 	}
+
 	public static void SortByDate(){
 		System.out.println("Sorting remaining WeatherEvents by date");
 	}
