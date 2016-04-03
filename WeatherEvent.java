106f4f8b0b180e20a23a83eac2956493d2d4c02e
Graham Van Deventer <gvandeventer124@gmail.com>
2016-04-02 20:51:10 -0400

    Updated comparison method

--- WeatherEvent.java
+++ WeatherEvent.java
@@ -1,6 +1,6 @@
 
 public class WeatherEvent {
-	private String county;
+	private String city;
 	private String state;
 	private int startMonth;
 	private int endMonth;
@@ -10,7 +10,7 @@ public class WeatherEvent {
 	private int year;
 	
 	public WeatherEvent(String c, String s, int sM, int eM, int sD, int eD, String eT, int y){
-		county = c;
+		city = c;
 		state = s;
 		startMonth = sM;
 		endMonth = eM;
@@ -23,10 +23,9 @@ public class WeatherEvent {
 		return this.eventType;
 	}
 	public String getDate(){
-		return (this.startMonth + "/" + this.startDay + "/" + this.year + " - " + 
-			this.endMonth + "/" + this.endDay + "/" + this.year);
+		return (this.startMonth + "/" + this.startDay + "/" + this.year + " - " + this.endMonth + "/" + this.endDay + "/" + this.year);
 	}
-	public String getLocation(){
+	public String getCounty(){
 		return(this.city);
 	}
 	public String getState(){
@@ -52,19 +51,26 @@ public class WeatherEvent {
 		}
 	}
 	public int compareLocation(WeatherEvent other){
-		int citycheck = this.county.compareToIgnoreCase(other.county); 
-		int statecheck = this.state.compareToIgnoreCase(other.state);
-		if (statecheck != 0){
-			return statecheck;
-		}
-		else{
-			return citycheck;
-		}
+		int citycheck = this.city.compareToIgnoreCase(other.city); 
+		//int statecheck = this.state.compareToIgnoreCase(other.state);
+		
+		return citycheck;
 	}
 	public int compareType(WeatherEvent other){
 		return this.eventType.compareToIgnoreCase(other.eventType);
 	}
 	public String toString(){
-		return (this.eventType + "\n" + this.county + ", " + this.state + "\n" + getDate());
+		return (this.city + ", " + this.state + ". " + this.getDate() + " " + this.getType());
+	}
+	public static void main(String[] args){
+		WeatherEvent test = new WeatherEvent("Hamilton","Ontario",1,1,3,4,"Blizzard",2016);
+		System.out.println(test.getDate());
+		System.out.println(test.getType());
+		System.out.println(test.toString());
+		WeatherEvent test2 = new WeatherEvent("Calgary","Alberta",2,2,3,6,"Extreme Cold",2016);
+		System.out.println(test.compareDate(test2));
+		System.out.println(test.compareLocation(test2) >= 0);
+		System.out.println(test.compareType(test2) >= 0);
+		
 	}
 }
