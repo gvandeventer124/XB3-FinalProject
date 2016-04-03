7cb8ffff88498e7fcf42ec678fbe6ae447b39dd9
Arfa <buttaa3@mcmaster.ca>
2016-04-03 17:07:39 -0400

    Update eventCalc.java
    Fixed it so each unique element is only printed once

--- eventCalc.java
+++ eventCalc.java
@@ -1,40 +1,40 @@
 package sortingForFinalProject;
-
 public class eventCalc{
 	
-	public void eventChance(WeatherEvent[] a){
-		int N = a.length;
-		String[] events = new String[N];
+	public static void eventChance(WeatherEvent[] a){
 		
+		int N = a.length;
+		int k = 0;
+		String[] events = new String[N];
+		String[] uniqueElements = new String[N];
+
 		for(int i = 0; i<N ;i++){
 			events[i] = a[i].getType();
 		}
-		int k = 0;
-		String[] uniqueElements = new String[N];
+
 		for(int i = 0; i<N; i++){
 			for(int j = 0; j<N; j++){
-				if(!events[i].equals(uniqueElements[j])){
+				if (events[i].equals(uniqueElements[j]))
+					break;
+				else if (uniqueElements[j] == null){
 					uniqueElements[k] = events[i];
 					k++;
 					break;
 				}
 			}
 		}
+		
 		for(int i =0; i<k; i++){
 			int count = 0;
-		for(int j = 0; j<N; j++){
-			if(uniqueElements[i].equals(events[j]) == true){
-				count ++;
+			for(int j = 0; j<N; j++){
+				if(uniqueElements[i].equals(events[j])){
+					count ++;
+				}
 			}
-			
-		}
-		double likely = (count*100)/N;
-		double rounded = Math.round(likely*100)/100;
-		System.out.println("chance of " +uniqueElements[i] + " = " + rounded + "% ");
+			double likely = (count*100.0/N);
+			likely = Math.round(likely * 1000.)/1000.;
+			System.out.println("chance of " +uniqueElements[i] + " = " + likely + "% ");
 		}
 		
 	}
-	
-	
-
 }
