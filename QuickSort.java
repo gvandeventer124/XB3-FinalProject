7d0f42a07c0cf5b28298a84af99c632f46ab57b5
adwity <me.adwity@hotmail.com>
2016-04-03 21:41:21 -0400

    Update QuickSort.java

--- QuickSort.java
+++ QuickSort.java
@@ -1,17 +1,12 @@
 package sortingForFinalProject;
 
-import java.security.SecureRandom;
-
 public class QuickSort {
 
-	private int[] a;
-
-	public void sort(int[] a) {
-		this.a = a;
+	public void sort(WeatherEvent[] a) {
 		sort(a, 0, a.length - 1);
 	}
 
-	public void sort(int[] a, int lo, int hi) {
+	public void sort(WeatherEvent[] a, int lo, int hi) {
 		if (hi <= lo)
 			return;
 		int j = partition(a, lo, hi);
@@ -19,23 +14,23 @@ public class QuickSort {
 		sort(a, j + 1, hi);
 	}
 
-	public int partition(int[] a, int lo, int hi) {
+	public int partition(WeatherEvent[] a, int lo, int hi) {
 		int i = lo, j = hi + 1;
-		int k = a[lo];
+		WeatherEvent k = a[lo];
 		while (true) {
-			while (a[++i] < k)
+			while (a[++i].compareDate(k) < 0)
 				if (i == hi)
 					break;
-			while (k < a[--j])
+			while (a[--j].compareDate(k) > 0)
 				if (j == lo)
 					break;
 			if (i >= j)
 				break;
-			int temp = a[i];
+			WeatherEvent temp = a[i];
 			a[i] = a[j];
 			a[j] = temp;
 		}
-		int t = a[lo];
+		WeatherEvent t = a[lo];
 		a[lo] = a[j];
 		a[j] = t;
 		return j;
